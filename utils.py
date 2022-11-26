0027ae21f1055d600394e38f21bcb3eefcc76b5e
qindalin <1278764563@qq.com>
2022-11-26 13:29:40 +0800

    add distance functions

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import torch
 import statsmodels.api as sm
 from statsmodels.distributions.empirical_distribution import ECDF
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import correlation
+from scipy.stats import wasserstein_distance
+from tslearn.metrics import dtw
 
 def autocor(data, lags, plot=True):
     # compute autocorrelation for lag=[0:lags]
@@ -19,3 +22,15 @@ def psd(data):
     # compute power spectral density
     pxx, freqs = plt.psd(data)
     return pxx, freqs
+
+def cor_dist(data_1, data_2):
+    # compute correlation distance between data_1 & data_2
+    return correlation(data_1, data_2)
+
+def ws_dist(data_1, data_2):
+    # compute wasserstein distance between data_1 & data_2
+    return wasserstein_distance(data_1, data_2)
+
+def dtw_dist(data_1, data_2):
+    # compute distance between data_1 & data_2 by Dynamic time warping
+    return dtw(data_1, data_2)
